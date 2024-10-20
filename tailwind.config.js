17ed77ca21d9be435b854cfd8112d3dd3e93f2f9
devusmonjon <usmonjonhasanov777@gmail.com>
2024-10-20 12:26:05 +0500

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#7000FF',
+        'secondary': '#FFFF00',
+        'primary-accent': '#E5E5FF',
+        'light': '#FFFFFF',
+        'light-secondary': '#7E818C',
+        'dark': '#1F2026',
+        'dark-secondary': '#4D4F59',
+        'border': 'rgba(54, 55, 64, 0.20)',
+        'product-bg': 'rgba(54, 55, 64, 0.10)',
+        'violet': '#2F006B',
+        'pink': '#FF3090',
+        'gray': '#E6E8ED',
+      },
+    },
   },
   plugins: [],
 }
